6fcfbdb252e3552bc28b470775260074c623ebfd
YBeksultan <82723103+YBeksultan@users.noreply.github.com>
2024-03-25 10:22:23 +0600

    proposals and afterGrading layouts done (need to add functionality and connect with api)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignInComponent from './components/signInComponents/SignInComponent';
 import Proposers from './components/mainPage/proposers';
 import Slider from './components/sliderComponent/sliderComponent';
+import Proposals from './components/proposalsComponent/proposalsComponent';
+import AfterGrading from './components/afterGradingComponent/afterGradingComponent';
 import Registration from './components/registrationPage/registrationComponent';
+
 
 function App() {
   return (
@@ -13,6 +16,8 @@ function App() {
           <Route path="/login" element={<SignInComponent />} />
           <Route path="/slider" element={<Slider />} />
           <Route path='/registration' element={<Registration/>}/>
+          <Route path='/proposals' element={<Proposals/>}/>
+          <Route path='/after_grading' element={<AfterGrading/>}/>
         </Routes>
     </Router>
   );
