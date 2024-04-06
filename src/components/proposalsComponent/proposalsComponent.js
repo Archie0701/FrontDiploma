3bb65471d382f2e676ecd639fedf7c6b9cd55099
YBeksultan <yerzhanov.beks@gmail.com>
2024-04-06 20:12:40 +0600

    menubar's position changed to fixed

--- src/components/proposalsComponent/proposalsComponent.js
+++ src/components/proposalsComponent/proposalsComponent.js
@@ -421,13 +421,6 @@ function MyComponent(props) {
             </svg>
           </Button5>
           </Link>
-          <MenuCollapse
-            loading="lazy"
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/6236b84bf174b42f3ae39f588cbc1551625088c8e0016e99592ad17058d2b3a8?apiKey=76bc4e76ba824cf091e9566ff1ae9339&"
-          ><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M7.55556 11.5556L7.55556 4.44444L4 8L7.55556 11.5556ZM16 14.2222C16 14.7111 15.8259 15.1296 15.4778 15.4778C15.1296 15.8259 14.7111 16 14.2222 16H1.77778C1.28889 16 0.87037 15.8259 0.522222 15.4778C0.174074 15.1296 0 14.7111 0 14.2222L0 1.77778C0 1.28889 0.174074 0.87037 0.522222 0.522222C0.87037 0.174073 1.28889 0 1.77778 0H14.2222C14.7111 0 15.1296 0.174073 15.4778 0.522222C15.8259 0.87037 16 1.28889 16 1.77778V14.2222ZM11.5556 14.2222H14.2222V1.77778H11.5556V14.2222ZM9.77778 14.2222V1.77778H1.77778L1.77778 14.2222H9.77778Z" fill="#5A5A5A" />
-            </svg>
-          </MenuCollapse>
         </Menu>
         <Div4>
           <Div5>
@@ -639,6 +632,9 @@ const Div2 = styled.div`
   }
 `;
 const Menu = styled.div`
+  position: fixed;
+  height: 100%;
+  z-index: 1;
   align-items: center;
   background-color: #fff;
   display: flex;
@@ -804,6 +800,7 @@ const MenuCollapse = styled.button`
 `;
 
 const Div4 = styled.div`
+  margin-left: 60px;
   align-self: start;
   display: flex;
   margin-top: 10px;
