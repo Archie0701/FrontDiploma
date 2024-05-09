417f762c34b3d0bb2cbfcd222571bb9aae268cca
YBeksultan <yerzhanov.beks@gmail.com>
2024-05-09 22:46:47 +0600

    changes for prod

--- next.config.js
+++ next.config.js
@@ -2,12 +2,19 @@ module.exports = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  webpack: (config, options) => {
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
   compiler: {
-    // Enables the styled-components SWC transform
     styledComponents: true
   }
-}
-
+};
 function throwError(envVar) {
   throw `Abort: You need to define ${envVar} in the .env file.`
 }
